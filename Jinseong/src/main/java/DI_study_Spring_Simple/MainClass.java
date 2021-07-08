050b8d70e41a9cd3e3d25020e01d108601b7b07a
jjsair0412 <jjsair0412@naver.com>
2021-07-08 18:30:05 +0900

    Spring Singleton and prototype

--- Jinseong/src/main/java/DI_study_Spring_Simple/MainClass.java
+++ Jinseong/src/main/java/DI_study_Spring_Simple/MainClass.java
@@ -8,9 +8,26 @@ public class MainClass {
 		AirToy airtoy = gen.getBean("airToy",AirToy.class);
 		airtoy.useAirMens();
 		
-		PersonToy pToy = gen.getBean("personToy",PersonToy.class);
-		pToy.myBattery();
-		pToy.UseMens();
+		PersonToy SpringToy = gen.getBean("personToy",PersonToy.class);
+		PersonToy Spring2Toy = gen.getBean("personToy",PersonToy.class);
+		
+		PersonToy javaToy = new PersonToy(new Battery());
+		PersonToy javaToy2 = new PersonToy(new Battery());
+		
+		if(SpringToy == Spring2Toy) {
+			System.out.println("둘은 같은 bean 가르키고있다.");
+		}else {
+			System.out.println("프로토타입설정 후 Spring도 아니다");
+		}
+	
+		if(javaToy == javaToy2) {
+			System.out.println("둘은 같은 객체를 가르키고있다.");
+		}else {
+			System.out.println("자바는 아니다.");
+		}
+		
+		SpringToy.myBattery();
+		SpringToy.UseMens();
 	
 	}
 }
